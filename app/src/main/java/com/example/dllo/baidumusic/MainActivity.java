84cc464826777ec4f64b01b33709d05c16a3fe62
dllo <815933539@qq.com>
2016-09-19 15:59:23 +0800

    图片

---
+++ app/src/main/java/com/example/dllo/baidumusic/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.dllo.baidumusic;
+
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
